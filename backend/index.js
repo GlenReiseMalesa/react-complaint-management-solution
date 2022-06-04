7543464e26d5f200e7143b41a10c939e4a13e3a1
GlenReiseMalesa <malesaglen6@gmail.com>
2022-06-04 04:34:28 +0200

    made updates to the login page

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,32 @@ app.post("/register",(req, res)=>{
 
 });
 
+//login query
+app.post("/login",(req, res)=>{
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password],
+        (err, result) =>{
+            
+           if(err){
+               res.send({ err: err});
+           }
+
+           if(result.length > 0){
+               res.send(result);
+           }else{
+               res.send({ message: "Wrong username/password!"});
+           }
+
+        }
+    );
+
+});
+
 
 
 app.listen(3001,()=>{
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,39 @@
 import '../App.css';
+import React,{ useState } from 'react';
 import {BrowserRouter ,Link,Navigate, Route ,Routes} from 'react-router-dom';
+import Axios from 'axios';
 
 
 function Login(){
 
 
+    const [userName, setUserName] = useState("");
+    const [passWord, setPassWord] = useState("");
+ 
+    const funcLogin = () =>{
+       Axios.post("http://localhost:3001/login",{
+          username: userName,
+          password: passWord,
+       }).then((response)=>{
+          
+
+          if(response.data.message){
+              //login failed
+              console.log(response.data.message);
+          }else{
+              //login successful
+              console.log(response.data[0].username);
+          }
+
+       })
+    };
 
    return(
        <div className="login">
            <h1>Login</h1>
-           <input type="email" placeholder='email..' />
-           <input type="password" placeholder='password..' />
-           <button>Login</button>
+           <input type="text" onChange={(e)=>{ setUserName(e.target.value) }} placeholder='username..' />
+           <input type="password" onChange={(e)=>{ setPassWord(e.target.value) }} placeholder='password..' />
+           <button onClick={ funcLogin }>Login</button>
            <Link to="/registration">haven't registered yet?</Link>
        </div>
    );
