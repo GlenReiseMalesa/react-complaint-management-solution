64eb8b9fe65bd85b1ef8f1381fb8f6a9ac587c0f
GlenReiseMalesa <malesaglen6@gmail.com>
2022-06-04 04:59:02 +0200

    made more changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Fragment} from 'react';
 import './App.css';
 import Login from './components/Login';
 import Register  from './components/Registration';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
          <Routes>
           <Route path='/' element={<Login />}></Route>
           <Route path='/registration' element={<Register />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
          </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import '../App.css';
+import React,{ useState } from 'react';
+import {BrowserRouter ,Link,Navigate, Route ,Routes} from 'react-router-dom';
+import Axios from 'axios';
+
+
+function Dashboard(){
+
+
+
+   return(
+       <div className="dashboard">
+
+       </div>
+   );
+}
+
+export default Dashboard;
