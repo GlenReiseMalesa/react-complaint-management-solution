f240082282781cfcef108b9f7c4e22fc84bbbc7d
GlenReiseMalesa <malesaglen6@gmail.com>
2022-06-07 05:18:12 +0200

    made updates to the navbar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@ import '../App.css';
 import React,{ useState,useEffect } from 'react';
 import {BrowserRouter ,Link,Navigate,useNavigate, Route ,Routes} from 'react-router-dom';
 import Axios from 'axios';
-
+import NavBar from './NavBar';
 
 function Dashboard(){
 
@@ -29,13 +29,7 @@ function Dashboard(){
 
                 if(response.data.message){
                 }else{
-                //get the data   
-                // let role = response.data[0].role;  
-                // console.log(role); 
-                // if(role.includes("Student")){
-                //     console.log(response.data[0]); 
-                // }    
-                 
+                //get the data                    
                 setContact(response.data[0].contact);
                 setEmailAddress(response.data[0].emailAddress);
                 setFirstName(response.data[0].firstName);
@@ -53,36 +47,15 @@ function Dashboard(){
     });
 
 
-    const loggedOut = ()=>{
-       sessionStorage.removeItem("isLoggedIn");
-       navigate('/');
-    }
-
 
 
    return(
        <div className="dashboard">
-            <div className="container">
-                <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-
-
-                <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
-                    <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
-                    <li><a href="#" className="nav-link px-2 link-dark">Pricing</a></li>
-                    <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
-                    <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
-                </ul>
-
-                <div className="col-md-3 text-end">
-                    <button type="button" onClick={loggedOut} className="btn btn-outline-primary me-2">Logout</button>
-                </div>
-                </header>
-            </div>
-
+            <NavBar role={userRole} />
             <div className="b-example-divider"></div>
        </div>
    );
+
 }
 
 export default Dashboard;
---
+++ src/components/NavBar.js
@@ -0,0 +1,83 @@
+import '../App.css';
+import React,{ useState,useEffect } from 'react';
+import {BrowserRouter ,Link,Navigate,useNavigate, Route ,Routes} from 'react-router-dom';
+import Axios from 'axios';
+
+
+function NavBar(props){
+
+    const navigate = useNavigate();
+    const loggedOut = ()=>{
+        sessionStorage.removeItem("isLoggedIn");
+        navigate('/');
+     }
+
+                 // let role = response.data[0].role;  
+                // console.log(role); 
+                // if(role.includes("Student")){
+                //     console.log(response.data[0]); 
+                // }     
+
+let role = props.role;
+
+if(role.includes("Student") && !role.includes("Admin")){
+    return(          
+        <div className="container">
+            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" className="nav-link px-2 link-secondary">VM-Complaints</a></li>
+            </ul>
+
+            <div className="col-md-3 text-end">
+                <button type="button" onClick={loggedOut} className="btn btn-outline-primary me-2">Logout</button>
+            </div>
+            </header>
+        </div>
+    );
+}
+
+if(role.includes("Admin") && !role.includes("Student")){
+    return(          
+        <div className="container">
+            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" className="nav-link px-2 link-secondary">VM-Complaints</a></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">New-Issues</Link></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">Completed-Issues</Link></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">Pending-Issues</Link></li>
+            </ul>
+
+            <div className="col-md-3 text-end">
+                <button type="button" onClick={loggedOut} className="btn btn-outline-primary me-2">Logout</button>
+            </div>
+            </header>
+        </div>
+    );
+}
+
+if(role.includes("Student")&&role.includes("Admin")){
+
+    return(          
+        <div className="container">
+            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" className="nav-link px-2 link-secondary">VM-Complaints</a></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">Create-Issue</Link></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">New-Issues</Link></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">Completed-Issues</Link></li>
+                <li><Link className="nav-link px-2 link-dark" to="/registration">Pending-Issues</Link></li>
+            </ul>
+
+            <div className="col-md-3 text-end">
+                <button type="button" onClick={loggedOut} className="btn btn-outline-primary me-2">Logout</button>
+            </div>
+            </header>
+        </div>
+    );
+}
+
+
+
+}
+
+export default NavBar;
